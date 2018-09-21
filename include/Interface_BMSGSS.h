49d129a8676c4b98b0c3fc075ffe45611cf37d6b
Luke-Heyfron <34146974+Luke-Heyfron@users.noreply.github.com>
2018-09-21 22:00:14 +0100

    Added GNU General Public License v3 (Part 2)

--- include/Interface_BMSGSS.h
+++ include/Interface_BMSGSS.h
@@ -1,3 +1,21 @@
+/*
+	TOpt: An Efficient Quantum Compiler that Reduces the T Count
+	Copyright (C) 2018  Luke Heyfron
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef INTERFACE_BMS_GSS_HEADER
 #define INTERFACE_BMS_GSS_HEADER
 
